c2607460fb01b8cd87d8806b4aec420ff528414e
arhab_safwan <asafwan72@gmail.com>
2023-06-12 16:05:23 +0600

    starting project

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+// Route::get('/notes', );         // to list all the notes of the users
+// Route::get('/notes/{note}', );  // to view the single users note and {note} will be the unique id
+// Route::get('/notes/create', );   // to create a note
+// Route::post('/notes',);         //to save a notes
+ 
+Route::resource('/notes',NoteController::class)->middleware(['auth']);  // this 4 work can be done in signle line with the help of controller
+                                                                        //->middleware(['auth']) is used to make sure none of the user cant login without authentication 
+
+
+require __DIR__.'/auth.php';
